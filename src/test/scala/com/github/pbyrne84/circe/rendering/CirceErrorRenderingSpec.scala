e89dc3392cfbe6d9da3c9010348f5c0ca3e8f7d1
Patrick Byrne <patrick.byrne@digital.hmrc.gov.uk>
2023-08-10 18:32:04 +0100

    Tagged type example

--- src/test/scala/com/github/pbyrne84/circe/rendering/CirceErrorRenderingSpec.scala
+++ src/test/scala/com/github/pbyrne84/circe/rendering/CirceErrorRenderingSpec.scala
@@ -2,8 +2,10 @@ package com.github.pbyrne84.circe.rendering
 
 import cats.data.{NonEmptyList, Validated}
 import io.circe
+import io.circe.Decoder.Result
+import io.circe.DecodingFailure.Reason.CustomReason
 import io.circe.parser.parse
-import io.circe.{parser, Decoder, Json}
+import io.circe.{parser, Decoder, DecodingFailure, HCursor, Json}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -85,10 +87,41 @@ class CirceErrorRenderingSpec extends AnyWordSpec with Matchers {
       case class C(cField1: String, cField2: String)
       case class B(bField1: String)
       case class A(aField1: String, aField2: C, aField3: B)
-      case class X(xField1: A, xField2: B, xField3: Boolean, xField4: List[String], xField5: D)
+      case class X(xField1: A, xField2: B, xField3: Boolean, xField4: List[String], xField5: D, age: TaggedAge)
       implicit val dDecoder: Decoder[D] = Decoder.decodeString.emap { value =>
         Left("custom error message")
       }
+
+      trait SafetyTag
+      type TaggedAge = Int with SafetyTag
+
+      class TaggedDecoder[From, To](attempt: From => Either[String, To])(implicit aDecoder: Decoder[From])
+          extends Decoder[To] {
+        override def apply(c: HCursor): Result[To] = {
+          c.as[From]
+            .flatMap(
+              value => attempt(value).left.map((error: String) => DecodingFailure(CustomReason(error), c))
+            )
+        }
+      }
+
+      object TaggedAge {
+
+        implicit val taggedAgeDecoder: Decoder[TaggedAge] =
+          new TaggedDecoder((possibleAge: Int) => attemptAge(possibleAge))
+
+        // You can generify all this code when doing many like we have created a generified decoder
+        def attemptAge(age: Int): Either[String, TaggedAge] = {
+          if (age < 0) {
+            Left(s"Age '$age' cannot be below zero")
+          } else {
+            Right(age.asInstanceOf[TaggedAge])
+          }
+        }
+      }
+
+      import TaggedAge.taggedAgeDecoder
+
       implicit val cDecoder: Decoder[C] = deriveDecoder[C]
       implicit val bDecoder: Decoder[B] = deriveDecoder[B]
       implicit val aDecoder: Decoder[A] = deriveDecoder[A]
@@ -103,7 +136,8 @@ class CirceErrorRenderingSpec extends AnyWordSpec with Matchers {
          |   },
          |   "xField3" : "",
          |   "xField4" : true,
-         |   "xField5" : "d"
+         |   "xField5" : "d",
+         |   "age" : -1
          |}""".stripMargin
       )
 
@@ -123,7 +157,8 @@ class CirceErrorRenderingSpec extends AnyWordSpec with Matchers {
           |  "xField2" : "$missingFieldErrorText",
           |  "xField3" : "Got value '\\"\\"' with wrong type, expecting 'true' or 'false'",
           |  "xField4" : "Got value 'true' with wrong type, expecting array",
-          |  "xField5" : "custom error message"
+          |  "xField5" : "custom error message",
+          |  "age" : "Age '-1' cannot be below zero"
           |}
         """.stripMargin
       )
